29c23ffd029ef24f0be486b2231c6ef3129e7176
Nima <nima.mohamed@hotmail.com>
2023-03-22 16:44:43 -0700

    made DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function DayListItem(props) {
+ const [day, setDay] = useState(props.name)
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
